b430be2c81f22bf7f9937c068eb955044adb6cb0
Daniiar <daniyarflash.m01@gmail.com>
2021-02-09 18:18:04 +0600

    Added authentication

---
+++ users/forms.py
@@ -0,0 +1,50 @@
+from django import forms
+from django.core.exceptions import ValidationError
+from django.contrib.auth import authenticate
+
+from users.models import User
+
+
+class RegisterForm(forms.ModelForm):
+    """
+    User registrations form
+    """
+
+    class Meta:
+        model = User
+        fields = [
+            'phone_number',
+            'first_name',
+            'last_name',
+            'username',
+            'email',
+            'password',
+        ]
+
+    def clean(self):
+        clean = super().clean()
+        email = clean.get('email')
+        try:
+            user = User.objects.get(email=email)
+            raise ValidationError('User already registered')
+        except User.DoesNotExist:
+            pass
+
+    def save(self, *args, **kwargs):
+        user = super().save()
+        user.set_password(self.cleaned_data['password'])
+        user.save()
+        return user
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField()
+
+    def authenticate_user(self):
+        user = authenticate(username=self.cleaned_data['username'], password=self.cleaned_data['password'])
+
+        if user:
+            return user.username
+        else:
+            return None
---
+++ users/views.py
@@ -0,0 +1,38 @@
+from django.http import HttpResponse
+from django.views import generic
+
+# Create your views here.
+from users.forms import RegisterForm, LoginForm
+
+
+class RegisterView(generic.CreateView):
+    form_class = RegisterForm
+    template_name = 'users/register.html'
+
+    def post(self, request, *args, **kwargs):
+        data = request.POST
+        form = self.form_class(data=data)
+
+        if form.is_valid():
+            form.save()
+            return HttpResponse('Yes you successfully registered!')
+        else:
+            return HttpResponse(f'Form is not valid! </br>{form.errors}')
+
+
+class LoginView(generic.FormView):
+    form_class = LoginForm
+    template_name = 'users/login.html'
+
+    def post(self, request, *args, **kwargs):
+        data = request.POST
+        form = self.form_class(data=data)
+
+        if form.is_valid():
+            login = form.authenticate_user()
+            if login:
+                return HttpResponse(f'Yes you successfully loggined {login}!')
+            else:
+                return HttpResponse('No such user!')
+        else:
+            return HttpResponse(f'Form is not valid! </br>{form.errors}')
